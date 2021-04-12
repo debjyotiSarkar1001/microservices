07aaf937845bd44eb46bb8b51d4df3527d396c12
debjyotiSarkar1001 <79792267+debjyotiSarkar1001@users.noreply.github.com>
2021-04-12 12:59:06 +0530

    Add files via upload

---
+++ eureka-discovery-server/src/main/java/com/cognizant/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package com.cognizant.eurekadiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+@SpringBootApplication
+@EnableEurekaServer
+@ComponentScan("com")
+public class EurekaDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+	}
+
+}
